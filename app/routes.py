3ee6b3c08c01e4e5203af97bb81425fb240094f8
williamle92 <william1992le@gmail.com>
2021-06-17 01:53:33 -0700

    added final codes to parse through the list

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask.json import jsonify
 from app import app
-from flask import render_template, request, Flask
+from flask import render_template, request
 from .models import Divvy
 
 
@@ -15,6 +15,13 @@ def index():
 def average():
     start = request.args.get('start')
     stop = request.args.get('stop')
-    trips = Divvy.query.filter(Divvy.start_time>start).filter(Divvy.stop_time<stop).all()
-    return jsonify({'trips': trips})
+    trips = Divvy.query.filter(Divvy.start_time > start).filter(Divvy.stop_time< stop).all()
+    average_trip = 0
+    for trip in trips:
+        average_trip += trip.trip_duration
+    average_trip /= len(trips)
+    return jsonify({ "averageDuration": average_trip})
+
+
+
 
